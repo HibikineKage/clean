a87e2f11b7eeba1379c7f7db068bc5bb53bdd39b
Hibikine Kage <goodsmile314159@gmail.com>
2018-03-26 03:41:26 +0900

    release 0.1.4

--- clean.py
+++ clean.py
@@ -1,5 +1,10 @@
 """Clean.py File Sorter."""
 from clean.cli import cli
 
+
+def clean_cli():
+    cli()
+
+
 if __name__ == '__main__':
-    cli()  # pylint: disable=E1120
+    clean_cli()  # pylint: disable=E1120
--- setup.py
+++ setup.py
@@ -2,15 +2,4 @@
 
 from setuptools import setup
 
-setup(
-    name='clean',
-    version='0.1.1',
-    py_modules=['clean'],
-    install_requires=[
-        'Click',
-    ],
-    entry_points='''
-        [console_scripts]
-        clean=clean:cli
-    ''',
-)
+setup()
